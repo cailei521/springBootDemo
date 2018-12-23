790946e1f375126a088c145cc345df6e9efda649
cailei <1029746679@qq.com>
2018-12-24 01:31:50 +0800

    我的更改

--- springboot_quick/src/main/java/com/huawei/SpringbootQuickApplication.java
+++ springboot_quick/src/main/java/com/huawei/SpringbootQuickApplication.java
@@ -9,6 +9,7 @@ public class SpringbootQuickApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootQuickApplication.class, args);
 	}
+	public void Test(){};
 
 }
 
